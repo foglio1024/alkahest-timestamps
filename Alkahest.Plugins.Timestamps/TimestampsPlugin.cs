e39399c36303a0c0b6ae97f78d323aad1d11a303
foglio <foglio1024@gmail.com>
2017-05-06 14:24:57 +0200

    added commands

--- Alkahest.Plugins.Timestamps/TimestampsPlugin.cs
+++ Alkahest.Plugins.Timestamps/TimestampsPlugin.cs
@@ -17,12 +17,16 @@ namespace Alkahest.Plugins.Timestamps
 
         static readonly Log _log = new Log(typeof(TimestampsPlugin));
 
+        bool showSeconds = true;
+        bool enabled = true;
+
         public void Start(GameProxy[] proxies)
         {
             foreach (var p in proxies.Select(x => x.Processor))
             {
                 p.AddHandler<SChatPacket>(HandleChat);
                 p.AddHandler<SPrivateChatPacket>(HandlePrivateChat);
+                p.AddHandler<CChatPacket>(HandleCommand);
                 //p.AddHandler<SWhisperPacket>(HandleWhisper);
             }
         }
@@ -32,6 +36,7 @@ namespace Alkahest.Plugins.Timestamps
             {
                 p.RemoveHandler<SChatPacket>(HandleChat);
                 p.RemoveHandler<SPrivateChatPacket>(HandlePrivateChat);
+                p.RemoveHandler<CChatPacket>(HandleCommand);
                 //p.AddHandler<SWhisperPacket>(HandleWhisper);
 
             }
@@ -39,23 +44,77 @@ namespace Alkahest.Plugins.Timestamps
 
         private bool HandleWhisper(GameClient client, Direction direction, SWhisperPacket packet)
         {
-            packet.SenderName = String.Format("</a>{0}][<a href='asfunction:chatNameAction,{1}@0@0'>{1}</a>", DateTime.Now.ToString("t"), packet.SenderName);
+            if (!enabled) return true;
+            string format = (showSeconds) ? "T" : "t";
+            packet.Message = String.Format("[{0}] {1}", DateTime.Now.ToString(format), packet.Message);
             client.SendToClient(packet);
             return false;
         }
 
+        private bool HandleCommand(GameClient client, Direction direction, CChatPacket packet)
+        {
+            var c = packet.Message.Replace("<FONT>", "");
+            c = c.Replace("</FONT>", "");
+            if (c.StartsWith(".ts "))
+            {
+                var arg0 = c.Substring(4);
+                if (arg0.Equals("on"))
+                {
+                    enabled = true;
+                    client.SendToClient(new SWhisperPacket() { SenderName = "timestamps-plugin", RecipientName="user", Message = "Timestamps on" });
+                    _log.Basic("Timestamps on");
+                }
+                else if (arg0.Equals("off"))
+                {
+                    enabled = false;
+                    client.SendToClient(new SWhisperPacket() { SenderName = "timestamps-plugin", RecipientName="user", Message = "Timestamps off" });
+                    _log.Basic("Timestamps off");
+                }
+                else if (arg0.StartsWith("sec"))
+                {
+                    var arg1 = arg0.Substring(4);
+                    if (arg1.Equals("on"))
+                    {
+                        showSeconds = true;
+                        client.SendToClient(new SWhisperPacket() { SenderName = "timestamps-plugin", RecipientName="user", Message = "Seconds on" });
+                        _log.Basic("Seconds on");
+
+
+                    }
+                    else if (arg1.Equals("off"))
+                    {
+                        showSeconds = false;
+                        client.SendToClient(new SWhisperPacket() { SenderName = "timestamps-plugin", RecipientName = "user", Message = "Seconds off" });
+                        _log.Basic("Seconds off");
+
+                    }
+                }
+                else
+                {
+
+                }
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
         private bool HandleChat(GameClient client, Direction direction, SChatPacket packet)
         {
-            if (packet.Channel == Core.Game.ChatChannel.Emote) return true;
-            packet.SenderName = String.Format("</a>{0}][<a href='asfunction:chatNameAction,{1}@0@0'>{1}</a>", DateTime.Now.ToString("t"), packet.SenderName);
+            if (packet.Channel == Core.Game.ChatChannel.Emote || !enabled) return true;
+            string format = (showSeconds) ? "T" : "t";
+            packet.SenderName = String.Format("</a>{0}][<a href='asfunction:chatNameAction,{1}@0@0'>{1}</a>", DateTime.Now.ToString(format), packet.SenderName);
             client.SendToClient(packet);
             return false;
         }
 
         private bool HandlePrivateChat(GameClient client, Direction direction, SPrivateChatPacket packet)
         {
-            packet.SenderName = String.Format("</a>{0}][<a href='asfunction:chatNameAction,{1}@0@0'>{1}</a>", DateTime.Now.ToString("t"), packet.SenderName);
+            if (!enabled) return true;
+            string format = (showSeconds) ? "T" : "t";
+            packet.SenderName = String.Format("</a>{0}][<a href='asfunction:chatNameAction,{1}@0@0'>{1}</a>", DateTime.Now.ToString(format), packet.SenderName);
             client.SendToClient(packet);
             return false;
         }
